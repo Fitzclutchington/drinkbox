a0229f4c5d0bfc0fa7ea550b1ede39eee1a51597
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2020-01-09 22:38:36 -0500

    first commit

---
+++ prototype_cli/game.py
@@ -0,0 +1,14 @@
+import termcolor
+def create_board():
+	board = []
+	board.append([''].extend(range(0,11))
+	return board
+    """
+	for row in range(11):
+		row_positions = []
+		for col in range(11):
+			row_positions.append
+	"""
+
+if __name__ == '__main__':
+	board = create_board()
